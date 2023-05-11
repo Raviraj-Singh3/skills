0bd1d31aee7d116bb62b097c0f398ee05141387c
Raviraj-Singh3 <iamtheraviraj@gmail.com>
2023-05-11 19:32:01 +0530

    first commit

---
+++ may11-23/dom-color-change/script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", changeRondomColor);
+
+function changeRondomColor(){
+    document.body.style.backgroundColor = randomColor();
+};
+
+    const randomColor = () => {
+        let val = "0123456789ABCDEF";
+        let cons = "#";
+        for(let i = 0; i < 6; i++ ){
+            cons = cons + val[Math.floor(Math.random() * 16)]
+            }
+        return cons ;
+    };
